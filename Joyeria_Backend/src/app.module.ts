b853bef31bbc02c321647c3d78f472baebd263dd
majo <maria.sandoval.o@ucb.edu.bo>
2025-12-02 23:21:01 -0400

    app module mongodb

--- Joyeria_Backend/src/app.module.ts
+++ Joyeria_Backend/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/BD2_Joyeria'), 
+    
+    ProductsModule,
+    CategoriesModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
