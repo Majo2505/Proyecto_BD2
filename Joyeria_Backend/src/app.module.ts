d932dbdd7d263e9f7c61a7d9ffaaeae695ab3e67
nazaret <nazaret.claure@ucb.edu.bo>
2025-12-03 11:11:28 -0400

    Update app.module.ts

--- Joyeria_Backend/src/app.module.ts
+++ Joyeria_Backend/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CartsModule } from './carts/carts.module';
+
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/BD2_Joyeria'), 
-    
+    MongooseModule.forRoot('mongodb://localhost/BD2_Joyeria'),
     ProductsModule,
     CategoriesModule,
+    CartsModule, 
   ],
   controllers: [],
   providers: [],
